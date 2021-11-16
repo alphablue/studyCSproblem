04afdf27168c443e9be3b1666d349bd0903af80d
Jin <imy123123@naver.com>
2021-11-17 00:01:53 +0900

    boj 15651 해결

--- kotlin/Boj_15651_k.kt
+++ kotlin/Boj_15651_k.kt
@@ -4,10 +4,36 @@ import kotlin.properties.Delegates
 class Boj_15651_k {
 
     private var N by Delegates.notNull<Int>()
+    private var M by Delegates.notNull<Int>()
+    private var selected by Delegates.notNull<MutableList<Int>>()
+    private var list = StringBuilder()
 
-    fun input() {
+    private fun input() {
         val scan = Scanner(System.`in`)
         N = scan.nextInt()
+        M = scan.nextInt()
+        selected = MutableList(M + 1) { 0 }
+    }
+
+    private fun recFunc(k: Int) {
+        if (k == M + 1) {
+            for (i in 1..M) list.append(selected[i]).append(" ")
+            list.append("\n")
+        } else {
+            for (i in 1..N) {
+                selected[k] = i
+
+                recFunc(k + 1)
+                selected[k] = 0
+            }
+        }
+    }
+
+    fun run() {
+        input()
+
+        recFunc(1)
+        println(list)
     }
 
 }
--- src/RunMain.kt
+++ src/RunMain.kt
@@ -1,5 +1,7 @@
 fun main() {
     val javaRun = Boj_15651()
+    val kotlinRun = Boj_15651_k()
 
-    javaRun.run()
+//    javaRun.run()
+    kotlinRun.run()
 }
