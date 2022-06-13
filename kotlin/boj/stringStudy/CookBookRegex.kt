be6c1299cef0bf1aedb481ed7676773e424da593
sjLee <imy123123@naver.com>
2022-06-13 20:15:46 +0900

    패스워드 정규식 적용

--- kotlin/boj/stringStudy/CookBookRegex.kt
+++ kotlin/boj/stringStudy/CookBookRegex.kt
@@ -1,10 +1,10 @@
 package boj.stringStudy
 
 class CookBookRegex {
-    val regex1 = """^([\da-z]+){6,13}&""".toRegex()
+    val regex1 = """(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+]).{6,13}""".toRegex()
 
     fun regexTest() {
-        println(regex1 matches "123123fasd")
+        println(regex1 matches "123er!")
     }
 }
 
