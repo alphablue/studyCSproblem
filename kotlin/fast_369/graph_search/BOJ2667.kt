9c034b1a48bbeb099f98723c00037140a4bc1868
sjLee <imy123123@naver.com>
2022-08-05 17:25:52 +0900

    날짜 계산 하는 방법 자료 조사 및 테스트

--- kotlin/fast_369/graph_search/BOJ2667.kt
+++ kotlin/fast_369/graph_search/BOJ2667.kt
@@ -1,6 +1,33 @@
 package fast_369.graph_search
 
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.concurrent.TimeUnit
+
 class BOJ2667 {
 
+    fun run() {
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val today: LocalDate = LocalDate.now()
+        val date: LocalDate = LocalDate.parse("20201122",formatter)
+        println(date)
+        println(today)
 
+        println(Period.between(
+            date,
+            today
+        ))
+
+        val test = Array<Int>(10){ data-> data + 1}
+        println(test)
+
+
+    }
+}
+
+fun main() {
+    BOJ2667().run()
 }
