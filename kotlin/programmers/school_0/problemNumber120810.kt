ee46f826560a6e5f6f4f42b355207df5d0b75394
sjLee <imy123123@naver.com>
2022-10-20 16:54:37 +0900

    [(programmers) 입문 문제 풀이]
     큰 어려움 없이 풀이가 진행되기 때문에 간단한 테스트 중

--- kotlin/programmers/school_0/problemNumber120810.kt
+++ kotlin/programmers/school_0/problemNumber120810.kt
@@ -1,7 +1,9 @@
 package programmers.school_0
 
+import java.util.Collections
 import kotlin.math.pow
 import kotlin.math.sqrt
+import kotlin.reflect.jvm.internal.impl.utils.CollectionsKt
 import kotlin.time.times
 import kotlin.time.toDuration
 
@@ -14,19 +16,23 @@ fun main(){
 
     val num1: Int = 5
     val num2: Int = 3
+    val test = intArrayOf(1,2,3)
 
     var result = 2.toDouble().pow(15)*7
+    val tt = StringBuffer()
     println(num2 / num1)
 
-    var result2 = "1f2d3s4a".toCharArray().sorted().joinToString("")
-    println(result2)
-
+    var result2 = "aabvb32123".toList().sorted()
+    val ddsds = test.toMutableList()
+    println(ddsds.toString())
+    println(result2.elementAt(0))
 
     val testArray = listOf(3, 2, 7, 4, 8, 1, 2, 3, 4, 5)
     val ddddf = intArrayOf(1, 3, 5)
     val ttt = testArray.sortedDescending().run { if(this.sum()>(this.first()*2)) 1 else 0}
-    val ddd = mutableSetOf<Pair<Int, Int>>()
-    println("a" !in result2)
+    val ddd = mutableSetOf<Int>()
+
+    println(testArray.subList(5, testArray.size))
 
     println(ddddf)
 }
